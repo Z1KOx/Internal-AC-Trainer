1304c0474abcd84c32fcf35fe6c9b6cc144c5ea6
Z1KOx <119358594+Z1KOx@users.noreply.github.com>
2023-11-23 08:09:39 +0100

    Updated Console

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <windows.h>
 #include <vector>
-#include <cstdio>
+#include <stdio.h>
 
 BOOL WINAPI HackThread(HMODULE hModule)
 {
@@ -8,7 +8,9 @@ BOOL WINAPI HackThread(HMODULE hModule)
 	FILE* f;
 	freopen_s(&f, "CONOUT$", "w", stdout);
 
-	printf("DLL injected in AssaultCube successfully\n\n");
+	printf("[STATUS] DLL injected in AssaultCube successfully\n\n");
+	printf("___________________________________________________\n\n");
+
 	printf("Numpad1 = Rifle Ammo\n");
 	printf("Numpad2 = Health\n");
 	printf("Numpad3 = Grenade\n");
@@ -18,11 +20,14 @@ BOOL WINAPI HackThread(HMODULE hModule)
 	printf("Numpad7 = Rifle Clip\n");
 	printf("END = Eject\n");
 
+	printf("___________________________________________________\n\n");
+	printf("Toggle section\n\n");
+
 	uintptr_t moduleBase = (uintptr_t)GetModuleHandle(L"ac_client.exe");
 
-	bool bAmmo{ false }, bHealth{false}, bGrenade{false}, bArmor{false}, bPistolAmmo{false}, bPistolClip{false}, bRifleClip{false};
+	bool bRifleAmmo{ false }, bHealth{ false }, bGrenade{ false }, bArmor{ false }, bPistolAmmo{ false }, bPistolClip{ false }, bRifleClip{ false };
 
-	while(true)
+	while (true)
 	{
 		if (GetAsyncKeyState(VK_END) & 1)
 		{
@@ -31,42 +36,77 @@ BOOL WINAPI HackThread(HMODULE hModule)
 
 		if (GetAsyncKeyState(VK_NUMPAD1) & 1)
 		{
-			bAmmo = !bAmmo;
+			bRifleAmmo = !bRifleAmmo;
+
+			if (!bRifleAmmo)
+				printf("Rifle Ammo toggle off\n");
+			else
+				printf("Rifle Ammo toggle on\n");
 		}
-		if(GetAsyncKeyState(VK_NUMPAD2) & 1)
+		if (GetAsyncKeyState(VK_NUMPAD2) & 1)
 		{
 			bHealth = !bHealth;
+
+			if (!bHealth)
+				printf("Health toggle off\n");
+			else
+				printf("Health toggle on\n");
 		}
 		if (GetAsyncKeyState(VK_NUMPAD3) & 1)
 		{
 			bGrenade = !bGrenade;
+
+			if (!bGrenade)
+				printf("Grenade toggle off\n");
+			else
+				printf("Grenade toggle on\n");
 		}
 		if (GetAsyncKeyState(VK_NUMPAD4) & 1)
 		{
 			bArmor = !bArmor;
+
+			if (!bArmor)
+				printf("Armor toggle off\n");
+			else
+				printf("Armor toggle on\n");
 		}
 		if (GetAsyncKeyState(VK_NUMPAD5) & 1)
 		{
 			bPistolAmmo = !bPistolAmmo;
+
+			if (!bPistolAmmo)
+				printf("Pistol Ammo toggle off\n");
+			else
+				printf("Pistol Ammo toggle on\n");
 		}
 		if (GetAsyncKeyState(VK_NUMPAD6) & 1)
 		{
 			bPistolClip = !bPistolClip;
+
+			if (!bPistolClip)
+				printf("Pistol Clip toggle off\n");
+			else
+				printf("Pistol Clip toggle on\n");
 		}
 		if (GetAsyncKeyState(VK_NUMPAD7) & 1)
 		{
 			bRifleClip = !bRifleClip;
+
+			if (!bRifleClip)
+				printf("Rifle Clip toggle off\n");
+			else
+				printf("Rifle Clip toggle on\n");
 		}
 
 		uintptr_t* localPlayerPtr = (uintptr_t*)(moduleBase + 0x0017E0A8);
 
-		if(localPlayerPtr)
+		if (localPlayerPtr)
 		{
-			if(bAmmo)
+			if (bRifleAmmo)
 			{
 				*(int*)(*localPlayerPtr + 0x140) = 999;
 			}
-			if(bHealth)
+			if (bHealth)
 			{
 				*(int*)(*localPlayerPtr + 0xEC) = 999;
 			}
