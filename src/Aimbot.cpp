5305944c3ca943470858ff29fecbd89c65950ae6
Z1KOx <119358594+Z1KOx@users.noreply.github.com>
2024-01-27 04:24:45 +0100

    Update Aimbot.cpp

--- src/Aimbot.cpp
+++ src/Aimbot.cpp
@@ -14,11 +14,7 @@ void Aimbot(uintptr_t moduleBase)
 	Vec3 localHeadPos = *(Vec3*)(localplayer + offsets::X_headPosition);
 	uintptr_t localTeam = *(uintptr_t*)(localplayer + offsets::Team);
 
-<<<<<<< HEAD
 	uintptr_t bestEntity = localplayer;
-=======
-	uintptr_t BestEntity = localplayer;
->>>>>>> 7821238166c5cb2234721295fe7d67d1257c116d
 	float distance = 1000;
 
 	for (int i = 1; i < playerCount; i++)
@@ -26,7 +22,6 @@ void Aimbot(uintptr_t moduleBase)
 		uintptr_t entity = *(uintptr_t*)(entityList + 0x4 * i);
 
 		uintptr_t entityTeam = *(uintptr_t*)(entity + offsets::Team);
-<<<<<<< HEAD
 		uintptr_t entityHealth = *(uintptr_t*)(entity + offsets::Health);
 		BYTE entityOnScreen = *(BYTE*)(moduleBase + offsets::EntityDrawen);
 		Vec3 entityHeadPos = *(Vec3*)(entity + offsets::X_headPosition);
@@ -54,10 +49,10 @@ void Aimbot(uintptr_t moduleBase)
 		float smooth = config::Value::aimbotSmoothness;
 		float fov = config::Value::aimbotFov;
 
-		if (GetAsyncKeyState(VK_CONTROL)) 
+		if (GetAsyncKeyState(VK_CONTROL))
 		{
 			if (std::abs(*(float*)(localplayer + offsets::X_view) - dx) <= fov &&
-				std::abs(*(float*)(localplayer + offsets::Y_view) - dy) <= fov) 
+				std::abs(*(float*)(localplayer + offsets::Y_view) - dy) <= fov)
 			{
 				*(float*)(localplayer + offsets::X_view) = (1.0 - smooth) * *(float*)(localplayer + offsets::X_view) + smooth * dx;
 				*(float*)(localplayer + offsets::Y_view) = (1.0 - smooth) * *(float*)(localplayer + offsets::Y_view) + smooth * dy;
@@ -65,28 +60,3 @@ void Aimbot(uintptr_t moduleBase)
 		}
 	}
 }
-=======
-		Vec3 entityHeadPos = *(Vec3*)(entity + offsets::X_headPosition);
-
-		if ((localHeadPos - entityHeadPos).hypo3() < distance && localTeam != entityTeam)
-		{
-			BestEntity = entity;
-			distance = (localHeadPos - entityHeadPos).hypo3();
-		}
-	}
-
-	Vec3 delta = (*(Vec3*)(BestEntity + offsets::X_headPosition) - localHeadPos);
-
-	float yaw = atan2f(delta.y, delta.x) * 180 / M_PI;
-	float hyp = sqrt(delta.x * delta.x + delta.y * delta.y);
-	float pitch = atan2f(delta.z, hyp) * 180 / M_PI;
-
-	pitch + 90;
-
-	if (GetAsyncKeyState(VK_CONTROL))
-	{
-		*(float*)(localplayer + offsets::X_view) = yaw + 90;
-		*(float*)(localplayer + offsets::Y_view) = pitch;
-	}
-}
->>>>>>> 7821238166c5cb2234721295fe7d67d1257c116d
